2d3fd270320ab84059f063c15e71ab6679419f8d
James Johnson <james.johnson280@gmail.com>
2020-09-15 08:17:11 -0400

    Add functionality to get access to spotify

--- src/util/spotify.js
+++ src/util/spotify.js
@@ -1,11 +1,21 @@
 let accessToken = undefined;
+const redirectURL = 'http://localhost:3000/';
+const clientID = 'bf460d4fa8b34901b82fcc312dbe1059';
 
 const Spotify = {
   getAccessToken() {
     if (accessToken) { return accessToken; }
     const url = window.location.href;
-    const token = url.match('/access_token=([^&]*)/');
+    let token = url.match('/access_token=([^&]*)/');
     const expires = url.match('/expires_in=([^&]*)/');
+    if (token && expires) {
+      accessToken = token;
+      window.setTimeout(() => accessToken = '', expires * 1000);
+      window.history.pushState('Access Token', null, '/');
+    } else {
+      const url = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURL}`;
+      window.location.href = url;
+    }
   }
 };
 
